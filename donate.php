9d6a848cbcac1e41b534c08d3692140eeadd54de
Malcolm Fitzgerald <git@businessdatasystems.co.nz>
2022-10-13 11:43:19 +1300

    initial commit

---
+++ donate.php
@@ -0,0 +1,20 @@
+#! /usr/bin/php
+
+# Malcolm Fitzgerald
+# 2022-10-13
+# 
+# Given a date, determine which URL to display
+
+
+
+
+$urls = (
+            "default" => "", 
+            "seasonal"=>""
+            );
+
+$currentDate = date("n.j", time()) ;
+
+
+
+            
