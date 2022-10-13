e6b876e5129fbfd5d49a1de2a0d82404f482e7ed
Malcolm Fitzgerald <git@businessdatasystems.co.nz>
2022-10-13 15:14:42 +1300

    Moving the deck chairs. Remove test code, add notes, tweak inputs, make changes to conform to PS1 style

--- donate.php
+++ donate.php
@@ -5,12 +5,10 @@
 # 
 # determine which URL to display, based on the UTC date
 
-function Redirect($url, $permanent = false) {
-    header('Location: ' . $url, true, $permanent ? 301 : 302);
-    exit();
-}
 
-$now = (int) time();
+/*
+ * provide a default target and set the current target to the default
+ */
 
 $default = ["target" => "https://www.zeffy.com/en-CA/donation-form/ada8ec6b-e24d-4486-be1a-bc57f7cfbf49"];
 $target = $default["target"];
@@ -19,13 +17,15 @@ $target = $default["target"];
  * Specifying events in the $seasonal array
  * - we may have more than one seasonal event, so we have an array
  * 
- * Date format is YYYY-MM-DD-H 
+ * Date format is YYYY-M-D-H, in PHP formatting rules that is "Y-n-d-h"
+ * Month, day and hour can be a single digit
  * If hour is not included the current time is used.
  * Setting H to zero is the same as 00:00:00
  * We are ignoring timezones. Time is UTC
  * */
 
 
+$now = (int) time();
 $seasonal = [
     [
         "label" => "Testing",
@@ -34,7 +34,7 @@ $seasonal = [
         "target" => "https://www.zeffy.com/en-CA/donation-form/bcec56fb-9c63-4243-8a51-433b049b9efd"
     ],
     [
-        "label" => "Annual Funding Drive",
+        "label" => "Annual Funding Drive 2022",
         "start" => "2022-11-6-0",
         "end" => "2022-12-18-0",
         "target" => "https://www.zeffy.com/en-CA/donation-form/bcec56fb-9c63-4243-8a51-433b049b9efd"
@@ -44,15 +44,9 @@ $seasonal = [
 foreach ($seasonal as $event) {
     $start = DateTime::createFromFormat("Y-n-d-h", $event["start"])->format("U");
     $end = DateTime::createFromFormat("Y-n-d-h", $event["end"])->format("U");
-
-    var_dump($end);
-    var_dump($start);
-    var_dump($now);
     $target = ( (int) $start < $now && $now < (int) $end) ? $event["target"] : $target;
 }
 
 
-
-
-Redirect($target, false);
-
+header('Location: ' . $target, true, 302);
+exit();
